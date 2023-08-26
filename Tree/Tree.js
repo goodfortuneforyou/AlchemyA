891c7a3d37527c317431d71f407721f1d83b0e2f
miodragstrak <44975850+miodragstrak@users.noreply.github.com>
2023-08-26 10:17:12 +0200

    first commit

---
+++ Tree/Tree.js
@@ -0,0 +1,63 @@
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    addNode(node) {
+        if(!this.root) {
+            this.root = node;
+            return;
+        }
+        
+        let ptr = this.root;
+        while(true) {
+            if(node.data < ptr.data) {
+                if(!ptr.left) {
+                    ptr.left = node;
+                    break;
+                }
+                else {
+                    ptr = ptr.left;
+                }
+            }
+            if (node.data > ptr.data) {
+                if (!ptr.right) {
+                    ptr.right = node;
+                    break;
+                }
+                else {
+                    ptr = ptr.right;
+                }
+            }
+        }
+    }
+
+    hasNode(number) {
+        let ptr = this.root;
+        while(true) {
+            if(number === ptr.data) {
+                return true;
+            }
+            else {
+                if (number < ptr.data) {
+                    if(!ptr.left) {
+                        return false;
+                    }
+                    else {
+                        ptr = ptr.left;
+                    }
+                }
+                else {
+                    if (!ptr.right) {
+                        return false;
+                    }
+                    else {
+                        ptr = ptr.right;
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = Tree;
